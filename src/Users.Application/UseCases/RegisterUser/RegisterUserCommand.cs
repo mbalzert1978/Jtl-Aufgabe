39d1a67915faa28cf3ba2660b1b8cf118ce224bf
Markus Iorio <teilen-01-koralle@icloud.com>
2025-10-24 14:45:06 +0200

    feat: implement RegisterUser command and handler for user registration functionality

---
+++ src/Users.Application/UseCases/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,15 @@
+// <copyright file="RegisterUserCommand.cs" company="Markus - Iorio">
+// Copyright (c) Markus - Iorio. All rights reserved.
+// </copyright>
+
+using Monads.Results;
+using SharedKernel.Abstractions;
+using Users.Application.Adapters;
+
+namespace Users.Application.UseCases.RegisterUser;
+
+/// <summary>
+/// Command to register a new user in the system.
+/// </summary>
+/// <param name="Username">The username for the new user.</param>
+public sealed record RegisterUserCommand(string Username) : ICommand<UserEntity>;
---
+++ src/Users.Application/UseCases/RegisterUser/RegisterUserHandler.cs
@@ -0,0 +1,61 @@
+// <copyright file="RegisterUserHandler.cs" company="Markus - Iorio">
+// Copyright (c) Markus - Iorio. All rights reserved.
+// </copyright>
+
+using System.Diagnostics;
+using Monads.Results;
+using Monads.Results.Extensions.Async;
+using SharedKernel.Abstractions;
+using Users.Application.Adapters;
+using Users.Domain;
+using Users.Domain.Abstractions;
+using Users.Domain.Models.Users;
+
+namespace Users.Application.UseCases.RegisterUser;
+
+/// <summary>
+/// Handles the registration of new users in the system.
+/// </summary>
+internal sealed class RegisterUserCommandHandler : ICommandHandler<RegisterUserCommand, UserEntity>
+{
+    private readonly IUserRepository _userRepository;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RegisterUserCommandHandler"/> class.
+    /// </summary>
+    /// <param name="userRepository">The repository for user persistence operations.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="userRepository"/> is null.</exception>
+    public RegisterUserCommandHandler(IUserRepository userRepository)
+    {
+        ArgumentNullException.ThrowIfNull(userRepository);
+        _userRepository = userRepository;
+
+        Debug.Assert(
+            _userRepository == userRepository,
+            "UserRepository instance was not set correctly."
+        );
+    }
+
+    /// <summary>
+    /// Handles the user registration command by creating and persisting a new user.
+    /// </summary>
+    /// <param name="command">The registration command containing user details.</param>
+    /// <param name="cancellationToken">Token to cancel the asynchronous operation.</param>
+    /// <returns>A result containing the created user entity or an error.</returns>
+    public async Task<Result<UserEntity, IError>> HandleAsync(
+        RegisterUserCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        Debug.Assert(command is not null, "Command must not be null.");
+
+        return await UserFactory
+            .Create(command.Username)
+            .BindAsync(user =>
+                _userRepository
+                    .AddAsync(user, cancellationToken)
+                    .MapAsync(_ => new UserEntity(user.Id, user.Username.Value))
+            )
+            .MapErrAsync(ApplicationErrorFactory.FromDomainError);
+    }
+}
