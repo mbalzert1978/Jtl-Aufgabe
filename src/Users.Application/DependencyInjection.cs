83994fc606834927889ab144ad168c3e5d05c2ac
Markus Iorio <teilen-01-koralle@icloud.com>
2025-10-24 14:45:32 +0200

    feat: implement dependency injection for user application layer

---
+++ src/Users.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SharedKernel;
+using SharedKernel.Abstractions;
+using Users.Application.Adapters;
+using Users.Application.UseCases.RegisterUser;
+using Users.Domain.Abstractions;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddUserApplicationLayer(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<
+            ICommandHandler<RegisterUserCommand, UserEntity>,
+            RegisterUserCommandHandler
+        >();
+        return services;
+    }
+}
