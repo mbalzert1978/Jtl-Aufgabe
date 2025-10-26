ade93fa7aeb7df2c1e6355d0cadaade8b97a652b
Markus Iorio <teilen-01-koralle@icloud.com>
2025-10-26 20:01:24 +0100

    feat: update DependencyInjection to use SQLite and add database creation logic

--- src/Users.Infrastructure/DependencyInjection.cs
+++ src/Users.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SharedKernel;
 using SharedKernel.Abstractions;
@@ -13,10 +14,24 @@ public static class DependencyInjection
     public static IServiceCollection AddUserInfrastructureLayer(this IServiceCollection services)
     {
         services.AddSingleton(TimeProvider.System);
-        services.AddDbContext<UsersDbContext>(opt => opt.UseInMemoryDatabase("UsersDb"));
-        services.AddScoped<IDatabase, UsersDbContext>(provider =>
-            provider.GetRequiredService<UsersDbContext>()
-        );
+        services.AddDbContext<UsersDbContext>(opt => opt.UseSqlite("Data Source=.users.db"));
+        services.AddScoped<IDatabase, UsersDbContext>(p => p.GetRequiredService<UsersDbContext>());
         return services;
+    }
+
+    public static async Task EnsureDatabaseCreated(
+        this IServiceProvider services,
+        CancellationToken ct = default
+    )
+    {
+        Debug.Assert(services is not null, "Service provider must not be null.");
+
+        using AsyncServiceScope scope = services.CreateAsyncScope();
+        Debug.Assert(scope.ServiceProvider is not null, "Scoped service provider must not be null.");
+
+        UsersDbContext dbContext = scope.ServiceProvider.GetRequiredService<UsersDbContext>();
+        Debug.Assert(dbContext is not null, "UsersDbContext must not be null.");
+
+        await dbContext.Database.EnsureCreatedAsync(ct).ConfigureAwait(false);
     }
 }
