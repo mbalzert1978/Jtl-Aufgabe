77cb9678a5e2e27c29dfa58166d1a7e90c4a9761
Markus Iorio <teilen-01-koralle@icloud.com>
2025-10-26 17:33:34 +0100

    feat: add Meta.cs to define assembly visibility for internal types

---
+++ src/SharedKernel/Meta.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tasks.Application")]
+[assembly: InternalsVisibleTo("Tasks.Domain")]
+[assembly: InternalsVisibleTo("Users.Application")]
+[assembly: InternalsVisibleTo("Users.Domain")]
