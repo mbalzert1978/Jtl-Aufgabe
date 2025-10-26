ced0af14986a8d3565a93955e34bf0f53cd77af1
Markus Iorio <teilen-01-koralle@icloud.com>
2025-10-26 17:39:32 +0100

    feat: update UserCreatedDomainEvent to include OccurredAt timestamp

--- src/Users.Domain/Models/Users/UserCreatedDomainEvent.cs
+++ src/Users.Domain/Models/Users/UserCreatedDomainEvent.cs
@@ -3,6 +3,7 @@
 // </copyright>
 
 using SharedKernel.Abstractions;
+using SharedKernel.Models.Common;
 
 namespace Users.Domain.Models.Users;
 
@@ -10,4 +11,7 @@ namespace Users.Domain.Models.Users;
 /// Domain event raised when a new user is created.
 /// </summary>
 /// <param name="UserId">The unique identifier of the created user.</param>
-internal sealed record UserCreatedDomainEvent(Guid UserId) : IDomainEvent;
+/// <param name="OccurredAt">The timestamp when the event occurred.</param>
+internal sealed record UserCreatedEvent(Guid UserId, DateTimeOffset OccurredAt)
+    : Event(Guid.NewGuid(), UserId, nameof(UserCreatedEvent), OccurredAt),
+        IDomainEvent;
