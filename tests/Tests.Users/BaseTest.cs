5c20d26b509efaa1bbd93ec210273670d0c38083
Markus Iorio <teilen-01-koralle@icloud.com>
2025-10-27 16:20:23 +0100

    feat: add architecture layer tests for Users and WorkItems projects

---
+++ tests/Tests.Users/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using JtlTask.WebApi.Features.Users.GetUserById;
+using Users.Application.Adapters;
+using Users.Domain.Models.Users;
+using Users.Infrastructure.Persistence;
+
+namespace Tests.Users;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(UserEntity).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(UsersDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(GetUserByIdQuery).Assembly;
+}
---
+++ tests/Tests.WorkItems/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using JtlTask.WebApi.Features.Users.GetUserById;
+using WorkItems.Application.Adapters;
+using WorkItems.Domain.Models.WorkItems;
+using WorkItems.Infrastructure.Persistence;
+
+namespace Tests.Users;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(WorkItem).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(WorkItemEntity).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(WorkItemsDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(GetUserByIdQuery).Assembly;
+}
