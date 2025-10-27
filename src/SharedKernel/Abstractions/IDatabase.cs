aca5f0b2293af4b92c77097799a8e2e08f709f25
Markus Iorio <teilen-01-koralle@icloud.com>
2025-10-27 12:35:50 +0100

    refactor: update IDatabase interface to include Query method for retrieving entities

--- src/SharedKernel/Abstractions/IDatabase.cs
+++ src/SharedKernel/Abstractions/IDatabase.cs
@@ -1,10 +1,29 @@
+// <copyright file="IDatabase.cs" company="Markus - Iorio">
+// Copyright (c) Markus - Iorio. All rights reserved.
+// </copyright>
+
 namespace SharedKernel.Abstractions;
 
+/// <summary>
+/// Defines the contract for database operations.
+/// </summary>
 public interface IDatabase
 {
-    Task<IEnumerable<TEntity>> GetAsync<TEntity>(CancellationToken cancellationToken = default)
+    /// <summary>
+    /// Retrieves a queryable collection of entities of the specified type.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity to retrieve.</typeparam>
+    /// <returns>An <see cref="IQueryable{T}"/> of the specified entity type.</returns>
+    IQueryable<TEntity> Query<TEntity>()
         where TEntity : class;
 
+    /// <summary>
+    /// Adds a new entity to the database.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity to add.</typeparam>
+    /// <param name="entity">The entity instance to add.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     Task AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default)
         where TEntity : class;
 }
