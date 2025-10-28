250072fe5dc1ceaf1e0e4c067cd184159f6cf6cf
Markus Iorio <teilen-01-koralle@icloud.com>
2025-10-28 12:00:23 +0100

    feat: add InternalsVisibleTo attributes for Tests.Users and Tests.WorkItems

--- src/SharedKernel/Meta.cs
+++ src/SharedKernel/Meta.cs
@@ -4,3 +4,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("WorkItems.Domain")]
 [assembly: InternalsVisibleTo("Users.Application")]
 [assembly: InternalsVisibleTo("Users.Domain")]
+[assembly: InternalsVisibleTo("Tests.Users")]
+[assembly: InternalsVisibleTo("Tests.WorkItems")]
